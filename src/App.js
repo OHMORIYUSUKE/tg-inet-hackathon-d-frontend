1273e1a13539cabdc92af22c046b1fa23f74432c
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-11-27 20:38:29 +0900

    yarn add react-router-dom@5.2.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
-import Index from "./Pages/Top";
+import Top from "./Pages/Top";
 import View from "./Pages/View";
 import NotFound from "./Pages/NotFound";
-import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   const history = useHistory();
   return (
-    <ChakraProvider>
-      <BrowserRouter history={history}>
-        <Switch>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/anime/:name" render={() => <View />} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </ChakraProvider>
+    <BrowserRouter history={history}>
+      <Switch>
+        <Route exact path="/" component={Top} />
+        <Route exact path="/anime/:name" render={() => <View />} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
